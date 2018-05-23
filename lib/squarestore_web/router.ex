eb927db6408cc33b0a6acbaf0a6680ddfacbde02
geirilja <geir.myhre@gmail.com>
2018-05-23 13:55:51 +0200

    made scope in elixir. Added button-style

--- lib/squarestore_web/router.ex
+++ lib/squarestore_web/router.ex
@@ -16,7 +16,8 @@ defmodule SquarestoreWeb.Router do
   scope "/", SquarestoreWeb do
     pipe_through :browser # Use the default browser stack
     get "/", PageController, :index
-	get "/userpage", UserPageController, :index
+  	get "/userpage", UserPageController, :index
+    get "/login", UserPageController, :index
   end
 
   # Other scopes may use custom stacks.
