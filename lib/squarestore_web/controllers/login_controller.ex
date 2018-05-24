68b3ff33d7096dea268213167671e68496d9febf
Sepukoo <37101970+Sepukoo@users.noreply.github.com>
2018-05-24 13:46:58 +0200

    fixed login.html (#17)
    * added reg user
    
    * completed reg_user
    
    * changed lindex.html to login.html
    changed login controller
    changed closefunction in login.html

--- lib/squarestore_web/controllers/login_controller.ex
+++ lib/squarestore_web/controllers/login_controller.ex
@@ -2,6 +2,6 @@ defmodule SquarestoreWeb.LoginController do
   use SquarestoreWeb, :controller
 
   def index(conn, _params) do
-    render conn, "index.html"
+    render conn, "login.html"
   end
 end
