88fecd5214ec8aca8e0e5a65ec704a1d5820b287
MrJohns1 <lykketrolle03@gmail.com>
2018-05-24 14:36:23 +0200

    rebase

--- lib/squarestore_web/controllers/reg_user_controller.ex
+++ lib/squarestore_web/controllers/reg_user_controller.ex
@@ -1,7 +1,7 @@
 defmodule SquarestoreWeb.RegUserController do
     use SquarestoreWeb, :controller
-
+  
     def index(conn, _params) do
-      render conn, "index.html"
+      render conn, "reg_user.html"
     end
   end
